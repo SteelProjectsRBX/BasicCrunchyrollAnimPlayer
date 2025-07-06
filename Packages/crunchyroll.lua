3e277791a69678238a34e83f3e5df08f35c4f6fd
KV2 <sirsinaki@hotmail.com>
2025-07-06 19:19:24 +0300

    Initial commit

---
+++ Packages/crunchyroll.lua
@@ -0,0 +1,9 @@
+local crunchyroll = require(script.Parent._Index["ffrostfall_crunchyroll@0.1.0"]["crunchyroll"])
+
+export type LimbInfo = crunchyroll.LimbInfo
+export type Rig = crunchyroll.Rig
+
+export type AnimationTrack = crunchyroll.AnimationTrack
+export type AnimationAsset = crunchyroll.AnimationAsset
+
+return crunchyroll
