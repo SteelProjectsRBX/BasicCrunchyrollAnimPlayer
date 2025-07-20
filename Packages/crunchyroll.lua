2c0358158c80d6191a6f528b2139c870a507f232
KV2 <sirsinaki@hotmail.com>
2025-07-20 19:41:02 +0300

    t

--- Packages/crunchyroll.lua
+++
@@ -1,6 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["ffrostfall_crunchyroll@0.1.0"]["crunchyroll"])
-export type LimbInfo = REQUIRED_MODULE.LimbInfo 
-export type Rig = REQUIRED_MODULE.Rig 
-export type AnimationTrack = REQUIRED_MODULE.AnimationTrack 
-export type AnimationAsset = REQUIRED_MODULE.AnimationAsset 
-return REQUIRED_MODULE
