3aaa45903f80fae205b4b241099e1383196e4663
KV2 <sirsinaki@hotmail.com>
2025-07-20 18:57:57 +0300

    Fix directories and stuff

--- Packages/crunchyroll.lua
+++ Packages/crunchyroll.lua
@@ -1,9 +1,6 @@
-local crunchyroll = require(script.Parent._Index["ffrostfall_crunchyroll@0.1.0"]["crunchyroll"])
-
-export type LimbInfo = crunchyroll.LimbInfo
-export type Rig = crunchyroll.Rig
-
-export type AnimationTrack = crunchyroll.AnimationTrack
-export type AnimationAsset = crunchyroll.AnimationAsset
-
-return crunchyroll
+local REQUIRED_MODULE = require(script.Parent._Index["ffrostfall_crunchyroll@0.1.0"]["crunchyroll"])
+export type LimbInfo = REQUIRED_MODULE.LimbInfo 
+export type Rig = REQUIRED_MODULE.Rig 
+export type AnimationTrack = REQUIRED_MODULE.AnimationTrack 
+export type AnimationAsset = REQUIRED_MODULE.AnimationAsset 
+return REQUIRED_MODULE
